79299e32eaf547100d7cb838a02775481cc4e05e
Xavier Bawerk <dpllgr@hotmail.com>
2019-08-14 00:21:07 -0500

    capitulo 10

--- 10-Files_and_exceptions/10.12_favourite_number.py
+++ 10-Files_and_exceptions/10.12_favourite_number.py
@@ -1,15 +1,15 @@
 import json
+try:
+    file_num = "C:\\Users\DopellGanger1\Desktop\Python Crash Course\\10-Files_and_exceptions\\number.json"
+    with open(file_num) as file:
+        fav_num =json.load(file)
 
-favourite_num = input("What's your favourite number?:" )
-file_num = "number.json"
-
-with open(file_num, "w") as file:
-    json.dump(favourite_num, file)
-
-
-file_num = "number.json"
-with open(file_num) as file:
-    fav_num =json.load(file)
-    if fav_num == 
-
-print(f"I know your favourite number is {fav_num} ")
+except FileNotFoundError: 
+    favourite_num = input("What's your favourite number?:" )
+    file_num = "C:\\Users\DopellGanger1\Desktop\Python Crash Course\\10-Files_and_exceptions\\number.json"
+    with open(file_num, "w") as file:
+        json.dump(favourite_num, file)
+    print("I will remember your favourite number")
+    
+else:
+    print(f"I know your favourite number is {fav_num} ")
