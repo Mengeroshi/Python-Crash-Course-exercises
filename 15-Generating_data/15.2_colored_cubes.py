e8326cc611c9bdba500ec0182dfcb988c7819ce7
mengeroshi <dpllgr@hotmail.com>
2019-10-05 11:51:27 -0500

    cubes.py and colored_cubes.py resolved

--- 15-Generating_data/15.2_colored_cubes.py
+++ 15-Generating_data/15.2_colored_cubes.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 
-input_values = range(1,5000)
+input_values = range(1,5001)
 cubes = [x**3 for x in input_values]
 
 plt.style.use("seaborn")
 
 fig, ax = plt.subplots()
-ax.plot(input_values, cubes, linewidth=3)
+ax.scatter(input_values, cubes, c=cubes, cmap=plt.cm.flag, s=10)
 
 # Set chart title and label axes.
 ax.set_title("Cube Numbers", fontsize=24)
