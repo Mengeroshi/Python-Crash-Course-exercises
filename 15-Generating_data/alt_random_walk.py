e4bc120be90375be6da9266f9d0a09668af4f9d7
mengeroshi <dpllgr@hotmail.com>
2019-10-06 01:26:18 -0500

    modified random walks and fill walk refactoritazion added

--- 15-Generating_data/alt_random_walk.py
+++ 15-Generating_data/alt_random_walk.py
@@ -16,8 +16,15 @@ class RandomWalk:
     
         # Keep taking steps until the walk reaches the desired length.
         while len(self.x_values) < self.num_points:
-            x_step = self.get_step()
-            y_step = self.get_step()
+        
+            # Decide which direction to go and how far to go in that direction.
+            x_direction = choice([1])
+            x_distance = choice([0, 1, 2, 3, 4, 5, 6, 7])
+            x_step = x_direction * x_distance
+        
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4, 5, 6, 7, 8])
+            y_step = y_direction * y_distance
         
             # Reject moves that go nowhere.
             if x_step == 0 and y_step == 0:
@@ -29,10 +36,3 @@ class RandomWalk:
         
             self.x_values.append(x)
             self.y_values.append(y)
-            
-    def get_step(self):
-        """Decide which direction to go and how far to go in that direction."""
-        direction = choice([1, -1])
-        distance = choice([0, 1, 2, 3, 4])
-        return direction * distance
-
