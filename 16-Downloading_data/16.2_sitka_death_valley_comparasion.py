a8cb016679137bc7b373736aac013c7ca8bbb82f
mengeroshi <dpllgr@hotmail.com>
2019-10-08 10:45:38 -0500

    sitka and death valley comparasion finished

--- 16-Downloading_data/16.2_sitka_death_valley_comparasion.py
+++ 16-Downloading_data/16.2_sitka_death_valley_comparasion.py
@@ -15,7 +15,10 @@ def get_weather_data(filename, dates, highs, lows, date_index, high_index,
             current_date = datetime.strptime(row[date_index], '%Y-%m-%d')
             try:
                 high = int(row[high_index])
+                high = (high-32) * (5/9)
+
                 low = int(row[low_index])
+                low = (low-32) * (5/9)
             except ValueError:
                 print(f"Missing data for {current_date}")
             else:
@@ -27,18 +30,26 @@ filename = '/home/mengeroshi/Escritorio/Python-Crash-Course-exercises/16-Downloa
 highs, lows, dates = [], [], []
 get_weather_data(filename, dates, highs, lows, date_index=2, high_index=5, low_index=6)
 
-# Plot the high and low temperatures.
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, highs, c='red', alpha=0.5)
 ax.plot(dates, lows, c='blue', alpha=0.5)
 plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
+#Death Vaalley
+filename = '/home/mengeroshi/Escritorio/Python-Crash-Course-exercises/16-Downloading_data/data/death_valley_2018_simple.csv'
+highs, lows, dates = [], [], []
+get_weather_data(filename, dates, highs, lows, date_index=2, high_index=4, low_index=5)
+
+ax.plot(dates, highs, c='red', alpha=0.5)
+ax.plot(dates, lows, c='blue', alpha=0.5)
+plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
+
 # Format plot.
-plt.title("Daily high and low temperatures - 2018", fontsize=24)
+plt.title("Sitka vs Death Valley high and low temperatures - 2018", fontsize=20)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
-plt.ylabel("Temperature (F)", fontsize=16)
+plt.ylabel("Temperature (C)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
 plt.show()
